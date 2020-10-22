773196f3a367f93a29757ca9f566c91a23ea71ae
ybueno16 <yuribueno27@gmail.com>
2020-10-22 19:44:55 -0300

    arquivo em c

---
+++ Calculadora.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+int main(int argc, char** argv) {
+	float a,b;
+    char o;
+        
+
+        printf("Digite o primeiro valor ");
+        scanf("%f", &a);
+
+        printf("Digite um dos operadores (+,-,*,/) ");
+        scanf(" %c", &o);
+
+        printf("Digite o segundo valor ");
+        scanf("%f", &b);
+
+	printf("O operador escolhido foi %c", o);
+	switch(o)
+	{
+		case '+':
+			printf("%.2f + %.2f = %.2f\n", a, b, a + b );
+		break;
+
+		case '-':
+			printf("%.2f - %.2f = %.2f\n", a, b, a - b );
+		break;
+
+		case '*':
+			printf("%.2f * %.2f = %.2f\n", a, b, a * b );
+		break;
+
+		case '/':
+			if(b == 0)
+				printf("Número inválido");
+			else
+				printf("%.2f / %.2f = %.2f\n", a, b, a / b );
+		break;
+
+		default:
+			printf("Operador invalido: \n");
+	} 
+
+    return 0;    
+} 
+
